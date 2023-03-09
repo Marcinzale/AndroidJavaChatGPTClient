23e8e2614bb83d3fc93e6fdd7c9a5716da60dd87
Marcin <marcinzale@gmail.com>
2023-03-09 20:47:53 +0100

    Added app icons

--- app/src/main/java/com/example/easychatgpt/MainActivity.java
+++ app/src/main/java/com/example/easychatgpt/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer YOUR_OPENAI_API_KEY")
+                .header("Authorization","Bearer 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")
                 .post(body)
                 .build();
 
