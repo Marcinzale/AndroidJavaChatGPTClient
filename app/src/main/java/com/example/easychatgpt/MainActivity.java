ea22af268c52eccade584f8f7ce6b0bd4c4237d7
Marcin <marcinzale@gmail.com>
2023-03-09 21:31:38 +0100

    Solved problems with app icons and OpenAI API key

--- app/src/main/java/com/example/easychatgpt/MainActivity.java
+++ app/src/main/java/com/example/easychatgpt/MainActivity.java
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
         RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
         Request request = new Request.Builder()
                 .url("https://api.openai.com/v1/completions")
-                .header("Authorization","Bearer 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")
+                .header("Authorization","Bearer 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")
                 .post(body)
                 .build();
 
